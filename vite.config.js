501ebe660361b95b50f6fcd7f016a0228772c6df
MahmoudEissaM <mahmoudessa8200@gmail.com>
2025-03-03 19:33:30 +0200

    Fix: Set port to 8080 for Azure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080, // تغيير البورت ليكون 8080 لـ Azure
+    host: true, // يضمن إن السيرفر يشتغل بشكل صحيح على Azure
+  }
 })
