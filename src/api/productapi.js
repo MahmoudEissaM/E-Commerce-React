707ac83b406a4d45c0e7ee661871a7859acaab4f
MahmoudEissaM <mahmoudessa8200@gmail.com>
2025-03-03 19:29:50 +0200

    Add .env and Edit api

--- src/api/productapi.js
+++ src/api/productapi.js
@@ -1,17 +1,11 @@
 import axios from "axios";
-const baseURL = "http://localhost:3005/products";
+
+const baseURL = import.meta.env.VITE_API_URL;
 
 const getAllProducts = () => axios.get(baseURL);
 const getProductById = (productId) => axios.get(`${baseURL}/${productId}`);
-const addNewProduct = (product) => axios.post(`${baseURL}`, product);
+const addNewProduct = (product) => axios.post(baseURL, product);
 const deleteProduct = (productId) => axios.delete(`${baseURL}/${productId}`);
-const editProduct = (productId, product) =>
-	axios.put(`${baseURL}/${productId}`, product);
+const editProduct = (productId, product) => axios.put(`${baseURL}/${productId}`, product);
 
-export {
-	getAllProducts,
-	getProductById,
-	addNewProduct,
-	deleteProduct,
-	editProduct,
-};
+export { getAllProducts, getProductById, addNewProduct, deleteProduct, editProduct };
