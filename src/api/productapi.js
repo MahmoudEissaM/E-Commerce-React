e56133a7e4bd454f27e24c45142d0561b1782b08
MahmoudEissaM <mahmoudessa8200@gmail.com>
2025-03-08 20:42:32 +0200

    Configured JSON Server for Vercel

--- src/api/productapi.js
+++ src/api/productapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = `${import.meta.env.VITE_API_URL}/json`;
 
 const getAllProducts = () => axios.get(baseURL);
 const getProductById = (productId) => axios.get(`${baseURL}/${productId}`);
@@ -9,3 +9,4 @@ const deleteProduct = (productId) => axios.delete(`${baseURL}/${productId}`);
 const editProduct = (productId, product) => axios.put(`${baseURL}/${productId}`, product);
 
 export { getAllProducts, getProductById, addNewProduct, deleteProduct, editProduct };
+
